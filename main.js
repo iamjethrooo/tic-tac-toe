061ed5b8c0463114d6cd3cd85ab3bb1897acff37
Jethro Estangki <estangkij@gmail.com>
2020-07-06 13:06:22 +0800

    Styled scoreboard and add modal when game ends

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const playerFactory = (name, letter) => {
-  return { name, letter };
+const playerFactory = (name, letter, score) => {
+  return { name, letter, score };
 };
 
 const gameBoard = (() => {
@@ -15,6 +15,8 @@ const gameBoard = (() => {
   // Scoreboard 1
   const name1 = document.createElement("div");
   name1.className = "player-name";
+  name1.contentEditable = true;
+  name1.spellcheck = false;
   name1.id = "player1-name";
   name1.innerText = "Player 1";
 
@@ -23,11 +25,10 @@ const gameBoard = (() => {
   score1.id = "player1-score";
   score1.innerText = "0";
 
-  scoreboard1.appendChild(name1);
-  scoreboard1.appendChild(score1);
-
   const name2 = document.createElement("div");
   name2.className = "player-name";
+  name2.contentEditable = true;
+  name2.spellcheck = false;
   name2.id = "player1-name";
   name2.innerText = "Player 2";
 
@@ -36,8 +37,45 @@ const gameBoard = (() => {
   score2.id = "player2-score";
   score2.innerText = "0";
 
+  name1.addEventListener("input", () => {
+    game.player1.name = name1.innerText;
+  });
+  name2.addEventListener("input", () => {
+    game.player2.name = name2.innerText;
+  });
+
+  scoreboard1.appendChild(name1);
+  scoreboard1.appendChild(score1);
   scoreboard2.appendChild(name2);
   scoreboard2.appendChild(score2);
+
+  const modal = document.createElement("div");
+  modal.id = "modal";
+  const winner = document.createElement("div");
+  winner.id = "winner";
+  const buttonContainer = document.createElement("div");
+  buttonContainer.id = "button-container";
+  const playAgain = document.createElement("button");
+  playAgain.id = "play-again";
+  playAgain.innerText = "Play Again";
+  const reset = document.createElement("button");
+  reset.id = "reset";
+  reset.innerText = "Reset";
+
+  playAgain.addEventListener("click", () => {
+    displayController.clearGrid();
+    modal.style.display = "none";
+  });
+
+  reset.addEventListener("click", () => {
+    game.player1.score = 0;
+    game.player2.score = 0;
+    displayController.updateScoreboard();
+    displayController.clearGrid();
+    modal.style.display = "none";
+  });
+  buttonContainer.appendChild(playAgain);
+  buttonContainer.appendChild(reset);
 
   const createBoard = (cells) => {
     for (let i = 0; i < cells; i++) {
@@ -48,6 +86,7 @@ const gameBoard = (() => {
     main.appendChild(scoreboard1);
     main.appendChild(board);
     main.appendChild(scoreboard2);
+    main.appendChild(modal);
   };
 
   const getCells = () => {
@@ -55,7 +94,17 @@ const gameBoard = (() => {
   };
 
   let cells = document.querySelectorAll(".cell");
-  return { createBoard, board, cells, getCells, score1 };
+  return {
+    createBoard,
+    board,
+    cells,
+    getCells,
+    score1,
+    score2,
+    modal,
+    winner,
+    buttonContainer,
+  };
 })();
 
 const displayController = (() => {
@@ -69,26 +118,45 @@ const displayController = (() => {
     });
   };
 
-  const showWinScreen = () => {};
+  const clearGrid = () => {
+    let cells = gameBoard.getCells();
+    cells.forEach((cell) => {
+      cell.innerText = "";
+    });
+  };
+
+  const updateScoreboard = () => {
+    gameBoard.score1.innerText = game.player1.score;
+    gameBoard.score2.innerText = game.player2.score;
+  };
+
+  const showWinScreen = () => {
+    gameBoard.winner.innerText = `${game.getCurrentPlayer()} wins!`;
+    gameBoard.modal.appendChild(gameBoard.winner);
+    gameBoard.modal.appendChild(gameBoard.buttonContainer);
+    gameBoard.modal.style.display = "flex";
+  };
 
   const claimCell = (cell) => {
     let value = cell.textContent;
     if (!value) {
-      cell.textContent = game.getCurrentPlayer();
+      cell.textContent = game.getCurrentLetter();
       if (game.checkWinner()) {
-        if (game.currentPlayer.letter == "X") {
-          gameBoard.score1.innerText++;
-        } else gameBoard.score2.innerText++;
+        if (cell.textContent == "X") {
+          game.player1.score++;
+        } else game.player2.score++;
+        updateScoreboard();
+        showWinScreen();
       } else game.togglePlayer();
     }
   };
 
-  return { enableButtons };
+  return { enableButtons, clearGrid, updateScoreboard };
 })();
 
 const game = (() => {
-  let player1 = playerFactory("Player 1", "X");
-  let player2 = playerFactory("Player 2", "O");
+  let player1 = playerFactory("Player 1", "X", 0);
+  let player2 = playerFactory("Player 2", "O", 0);
   let currentPlayer = player1;
 
   const checkLine = (board, startCol, colDiff) => {
@@ -138,14 +206,20 @@ const game = (() => {
 
   const togglePlayer = () => {
     if (currentPlayer == player1) {
+      console.log("Toggle1");
       currentPlayer = player2;
     } else {
+      console.log("Toggle2");
       currentPlayer = player1;
     }
   };
 
-  const getCurrentPlayer = () => {
+  const getCurrentLetter = () => {
     return currentPlayer.letter;
+  };
+
+  const getCurrentPlayer = () => {
+    return currentPlayer.name;
   };
 
   const init = () => {
@@ -160,6 +234,7 @@ const game = (() => {
     checkWinner,
     togglePlayer,
     getCurrentPlayer,
+    getCurrentLetter,
   };
 })();
 
